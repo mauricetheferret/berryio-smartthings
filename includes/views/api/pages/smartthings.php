3bdea6b7a23ecc20bcc6126de8accd39aac3e21a
Ledridge <steve.ledridge@gmail.com>
2015-04-19 14:20:44 -0700

    Update smartthings.php

--- includes/views/api/pages/smartthings.php
+++ includes/views/api/pages/smartthings.php
@@ -1,10 +1,10 @@
 {
-  "1M":"<?=$load_average[0]?>",
-  "5M":"<?=$load_average[1]?>",
-  "15M":"<?=$load_average[2]?>",
-  "temp":"<?=$temperature?>",
-  "speed":"<?=$speed?>",
-  "volts":"<?=$voltage?>",
-  "perc":"<?=$perc?>"
+  "1M_load":"<?=$load_average[0]?>",
+  "5M_load":"<?=$load_average[1]?>",
+  "15M_load":"<?=$load_average[2]?>",
+  "cpu_temp":"<?=$temperature?>",
+  "cpu_speed":"<?=$speed?>",
+  "cpu_volt":"<?=$voltage?>",
+  "cpu_perc":"<?=$perc?>"
 }
 
