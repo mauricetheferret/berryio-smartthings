f1b338cd43de3643d50fb8bcee101527aabdd996
Ledridge <steve.ledridge@gmail.com>
2015-04-19 13:12:15 -0700

    Create smartthings.php
    Adding smartthings api

---
+++ includes/views/api/pages/smartthings.php
@@ -0,0 +1,11 @@
+"smartthings" 
+  {
+  "1M":"<?=$load_average[0]?>",
+  "5M":"<?=$load_average[1]?>",
+  "15M":"<?=$load_average[2]?>",
+  "temp":"<?=$temperature?>",
+  "speed":"<?=$speed?>",
+  "volts":"<?=$voltage?>",
+  "perc":"<?=$perc?>"
+  }
+
