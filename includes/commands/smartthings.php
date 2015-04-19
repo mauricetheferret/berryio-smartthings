b2a62b4148a60f75b122996dd8f14a1b6894f41f
Ledridge <steve.ledridge@gmail.com>
2015-04-19 12:56:00 -0700

    Create smartthings.php
    Adding new smartthings api

---
+++ includes/commands/smartthings.php
@@ -0,0 +1,34 @@
+
+<?                                                                                                  
+
+/*------------------------------------------------------------------------------                    
+  BerryIO CPU Status Command                                                                        
+------------------------------------------------------------------------------*/                    
+
+$title = 'CPU Status';                                                                              
+
+// Load the cpu functions                                                                           
+require_once(FUNCTIONS.'cpu.php');                                                                  
+
+// Load the system settings                                                                         
+settings('system', 1);                                                                              
+
+// Get the cpu details                                                                              
+$page['temperature'] = cpu_get_temp();                                                              
+$page['speed'] = cpu_get_speed();                                                                   
+$page['voltage'] = cpu_get_volts();                                                                 
+$page['load_average'] = sys_getloadavg();                                                           
+$page['perc'] = cpu_get_percentage();                                                               
+
+// Display status page                                                                              
+$GLOBALS['JAVASCRIPT']['common'] = 'common';                                                        
+$GLOBALS['JAVASCRIPT']['updateCPU'] = 'updateCPU';                                                  
+require_once(FUNCTIONS.'graph.php');                                                                
+$content .= view('pages/cpu_status', $page);                                                        
+
+// Check for missing information                                                                    
+if($page['temperature'] === FALSE || $page['speed'] === FALSE || $page['voltage'] === FALSE)        
+{                                                                                                   
+  $content .= message('WARNING: Unable retrieve all the CPU information');                          
+  return FALSE;                                                                                     
+}               
